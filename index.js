973e437a2a70e77f3bb89ed2bcc7757c5fb649ef
Emmanuel Multiverse <EJLuis2001+verizon@gmail.com>
2025-05-12 14:31:39 -0500

    asynnc workshop

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ require('dotenv').config();
 
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
+
+const getImage = async (query) => {
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=1&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+    const data = await fetch(endpoint);
+    const dataJson = await data.json();
+    return dataJson["data"]["url"];
+}
+
+getImage("cats");
